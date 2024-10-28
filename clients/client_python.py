a693c88ec180d3620cdfb61498c48839475df2b1
Roman Ishchenko <rishchenko@deloittece.com>
2024-10-28 16:31:45 +0200

    Moved clients into own folder

--- clients/client_python.py
+++ clients/client_python.py
@@ -8,7 +8,7 @@ url = "http://127.0.0.1:8000/nn_inference"
 photo_id = "043"
 
 
-file_path = f"./dataset/PH2Dataset/PH2 Dataset images/IMD{photo_id}/IMD{photo_id}_Dermoscopic_Image/IMD{photo_id}.bmp"  
+file_path = f"../dataset/PH2Dataset/PH2 Dataset images/IMD{photo_id}/IMD{photo_id}_Dermoscopic_Image/IMD{photo_id}.bmp"  
 output_path = "segmenataion.bmp"  
 
 
